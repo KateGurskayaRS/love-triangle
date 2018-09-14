877002f3f830b8430c1a439dd25791e5471f1c5b
KateGurskaya <kategurik22@gmail.com>
2018-09-14 22:48:33 +0300

    100/100

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+ for (var i = 0; i < preferences.length; i++) {
+   if (i != preferences[i]-1 && i == preferences[preferences[preferences[i]-1]-1]-1) 
+   count++;
+ }
+ return count/3;
 };
